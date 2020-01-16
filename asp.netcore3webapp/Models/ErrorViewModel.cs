57a274c0384e0dc06f4cb6bb50e6920acb14c2ea
unknown <chandu_mtl@yahoo.com>
2020-01-16 17:12:01 +0530

    first commit

---
+++ asp.netcore3webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace asp.netcore3webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
